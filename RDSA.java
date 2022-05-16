10ad32ee604d29c5d60b850aa89cddd430a53279
Meghanamaturi <meghanamaturi2748@gmail.com>
2022-05-16 20:10:39 +0530

    RDSA file added

---
+++ RDSA.java
@@ -0,0 +1,30 @@
+// RDSA : Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int x = nums.length;
+        int i,j,k,z=0,m=0;
+        for( i =0; i< x;i++)
+        {
+            k = nums[i];
+             if(nums[i]!=  2147483647){
+        for( j = i+1; j < x; j++ ){
+           
+            if(k == nums[j]) {
+               nums[j] =  2147483647;
+           }
+            }
+        }
+        }
+        for(int y =0;y<x;y++){
+            if(nums[y]!= 2147483647){
+                nums[z]= nums[y];
+                z++;
+                m++;
+            }
+        }
+        
+        return m;
+    }
+}
